fd7b3c7d57bdc90d7d7f274001a288aec8dda2be
Muneeb ur rehman <urrehmanm51@gmail.com>
2025-03-10 19:55:09 +0530

    calculator

--- calculator.js
+++ calculator.js
@@ -1,24 +1,34 @@
 
+let display=document.querySelector(".display")
+let button=document.querySelectorAll('button')
+
+let string=""
+
+button=Array.from(button)
 
 
 
 
+button.forEach((v)=>{
 
+v.addEventListener("click",(e)=>{
+if(e.target.innerHTML=="DEL"){
+string=string.slice(0,string.length-1)
+display.value=string
+}else if(e.target.innerHTML=="AC"){
+string=""
+display.value=string
+}else if(e.target.innerHTML=="="){
+string=eval(string)
+display.value=string
+}
+else{        
+string+=e.target.innerHTML
+display.value=string
+}
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})
 
 
 
